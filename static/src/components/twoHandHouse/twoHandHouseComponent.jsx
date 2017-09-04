821144248e9daa5420b9f82a767be86cb16bab7f
niceyuanze <niceyuanze@gmail>
2017-09-04 16:15:04 +0800

    add TwoHandHouseComponent router

---
+++ static/src/components/twoHandHouse/twoHandHouseComponent.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import 'antd/dist/antd.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import { Menu, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;;
+
+
+class TwoHandHouseComponent extends Component {
+	
+	render(){
+		
+		return (
+			<div>
+				111111111111111111111111
+			</div>
+		)
+	}
+	
+	
+	
+}
+
+export  default TwoHandHouseComponent;
