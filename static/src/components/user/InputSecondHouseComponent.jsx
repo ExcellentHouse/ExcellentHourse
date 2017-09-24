255894fe1926f527a9b43d9acd79a5e026aef576
niceyuanze <niceyuanze@gmail.com>
2017-09-24 14:52:22 +0800

    backup

--- static/src/components/user/InputSecondHouseComponent.jsx
+++ static/src/components/user/InputSecondHouseComponent.jsx
@@ -1,8 +1,10 @@
 import '../../static/css/twoHandHouse.css';
 import 'antd/dist/antd.css';
+import reqwest from 'request';
 import React, { Component } from 'react';
-import { Upload, Icon, Modal,Form,Radio,Row, Col, Menu,Input, Button, Checkbox,InputNumber,Cascader,Select } from 'antd';
+import { Upload, message,Icon, Modal,Form,Radio,Row, Col, Menu,Input, Button, Checkbox,InputNumber,Cascader,Select } from 'antd';
 const { TextArea } = Input;
+
 
 
 
@@ -24,7 +26,7 @@ class InputSecondHouseComponent extends Component {
         wrapperCol:{offset:5}
     }
 
-    handleChange(value) {
+    handleCommunitySelectChange(value) {
         console.log(`selected ${value}`);
     }
 
@@ -41,6 +43,8 @@ class InputSecondHouseComponent extends Component {
             defaultCommunity:''
         };
         this.areaCascaderChange = this.areaCascaderChange.bind(this);
+        this.testClick = this.testClick.bind(this);
+
 
     }
 
@@ -72,15 +76,87 @@ class InputSecondHouseComponent extends Component {
             .catch(e => console.log("Oops, error", e));
     }
 
+    handleCancel = () => this.setState({ previewVisible: false })
+
+    handlePreview = (file) => {
+        this.setState({
+            previewImage: file.url || file.thumbUrl,
+            previewVisible: true,
+        });
+    }
+
+    handleChange = ({ fileList }) => {
+        this.setState({ fileList })
+    }
+
+    testClick(){
+
+        const { fileList } = this.state;
+        console.log(fileList);
+        const formData = new FormData();
+        // formData.append('file',fileList[0]);
+        // console.log(fileList[0]);
+        fileList.forEach((file) => {
+            formData.append('files[]', file['originFileObj']);
+        });
+        formData.append('secondHandHouseInputDTO',{one:'1',two:'2',three:'3'});
+        fetch("/secondHandHouse/batchupload", {
+            method: "POST",
+                credentials: "include",
+                headers: {
+            },
+            body: formData
+        })
+    }
 
 
-    render() {
 
+
+
+
+    render(){
+        const { previewVisible, previewImage, fileList } = this.state;
+        const uploadButton = (
+            <div>
+                <Icon type="plus" />
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
 
 
         return (
             <div>
                 <Form>
+
+
+
+
+                    <Form.Item
+                        {...this.formItemLayout}
+                        label="房屋图片:"
+                    >
+
+
+                        <Upload
+                            action="/file/upload"
+                            listType="picture-card"
+                            fileList={fileList}
+                            onPreview={this.handlePreview}
+                            onChange={this.handleChange}
+                        >
+                            {fileList.length >= 5 ? null : uploadButton}
+                        </Upload>
+                        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                            <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                        </Modal>
+                        <Button onClick={this.testClick}>test</Button>
+                    </Form.Item>
+
+
+
+
+
+
                     <Form.Item
                         {...this.formItemLayout}
                         label="地址:"
@@ -95,7 +171,7 @@ class InputSecondHouseComponent extends Component {
                         {...this.formItemLayout}
                         label="小区:"
                     >
-                        <Select  placeholder="请选择小区"  style={{ width: 120 }} onChange={this.handleChange}>
+                        <Select  placeholder="请选择小区"  style={{ width: 120 }} onChange={this.handleCommunitySelectChange}>
                             {this.state.communityOptions}
                         </Select>
                     </Form.Item>
